2d8ae5d0fec61527513ad06dc11de1044c3816d6
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-01-26 18:39:42 -0500

    Load json into models

--- photos/views.py
+++ photos/views.py
@@ -3,9 +3,41 @@ import json
 from . import api
 from .models import Photo, Camera, Rover
 from typing import List
+from django.utils.dateparse import parse_date
 
-def load_json(photos: List[dict]):
-  print(Photo())
+def load_json(photos: List[dict]) -> List[Photo]:
+  def load_photo(obj: dict) -> Photo:
+    return Photo(
+      id=obj['id'],
+      sol=obj['sol'],
+      img_src=obj['img_src'],
+      earth_date=parse_date(obj['earth_date']),
+      camera_id=load_camera(obj['camera']),
+    )
+
+  def load_camera(obj: dict) -> Camera:
+    return Camera(
+      id=obj['id'],
+      name=obj['name'],
+      full_name=obj['full_name'],
+      # rover_id=obj['rover_id'],
+    )
+
+  def load_rover(obj: dict) -> Rover:
+    return Rover(
+      id=obj['id'],
+      name=obj['name'],
+      landing_date=parse_date(obj['landing_date']),
+      launch_date=parse_date(obj['launch_date']),
+      status=obj['status'],
+      max_sol=obj['max_sol'],
+      total_photos=obj['total_photos']
+    )
+
+  return [
+    load_photo(obj)
+    for obj in photos
+  ]
   pass
 
 def index(request):
@@ -13,5 +45,5 @@ def index(request):
     'sol': 1000,
     'page': 1
   })
-  load_json(photos)
-  return HttpResponse(json.dumps(photos))
+  loaded = load_json(photos)
+  return HttpResponse( loaded)
