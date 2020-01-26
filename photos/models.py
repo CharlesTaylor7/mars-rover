d35f365f209d02fd766a628cc8a821f0888db972
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-01-26 18:04:08 -0500

    Order models by constraint

--- photos/models.py
+++ photos/models.py
@@ -1,18 +1,5 @@
 from django.db import models
 
-class Photo(models.Model):
-  id = models.IntegerField(primary_key=True)
-  sol = models.IntegerField()
-  img_src = models.CharField(max_length=200)
-  earth_date = models.DateField(default=None)
-  camera_id = models.ForeignKey(Camera, on_delete=models.SET_NULL, null=True)
-
-class Camera(models.Model):
-  id = models.IntegerField(primary_key=True)
-  name = models.CharField(max_length=50)
-  full_name = models.CharField(max_length=200)
-  rover_id = models.ForeignKey(Rover, on_delete=models.SET_NULL, null=True)
-
 class Rover(models.Model):
   id = models.IntegerField(primary_key=True)
   name = models.CharField(max_length=200)
@@ -22,3 +9,16 @@ class Rover(models.Model):
   max_sol = models.IntegerField()
   max_date = models.DateField(default=None)
   total_photos = models.IntegerField()
+
+class Camera(models.Model):
+  id = models.IntegerField(primary_key=True)
+  name = models.CharField(max_length=50)
+  full_name = models.CharField(max_length=200)
+  rover_id = models.ForeignKey(Rover, on_delete=models.SET_NULL, null=True)
+
+class Photo(models.Model):
+  id = models.IntegerField(primary_key=True)
+  sol = models.IntegerField()
+  img_src = models.CharField(max_length=200)
+  earth_date = models.DateField(default=None)
+  camera_id = models.ForeignKey(Camera, on_delete=models.SET_NULL, null=True)
