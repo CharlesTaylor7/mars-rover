968c887fa3454de65d4f53d45c3352809795ceef
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-01-26 19:16:18 -0500

    Save models to db

--- photos/api.py
+++ photos/api.py
@@ -4,14 +4,25 @@ import os
 def api_key():
   return os.environ['NASA_API_KEY']
 
-def getPhotos(query):
-  query = {
+def get_photos(rover = '', **query):
+  query_params = {
     'api_key': api_key(),
     **query,
   }
   payload = requests.get(
-    'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
-    params = query
+    f'https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos',
+    params = query_params
   ).json()
 
   return payload['photos']
+
+def get_rovers():
+  query_params = {
+    'api_key': api_key(),
+  }
+  payload = requests.get(
+    f'https://api.nasa.gov/mars-photos/api/v1/rovers',
+    params = query_params
+  ).json()
+
+  return payload['rovers']
--- photos/views.py
+++ photos/views.py
@@ -5,45 +5,51 @@ from .models import Photo, Camera, Rover
 from typing import List
 from django.utils.dateparse import parse_date
 
-def load_json(photos: List[dict]) -> List[Photo]:
-  def load_photo(obj: dict) -> Photo:
-    return Photo(
-      id=obj['id'],
-      sol=obj['sol'],
-      img_src=obj['img_src'],
-      earth_date=parse_date(obj['earth_date']),
-      camera_id=load_camera(obj['camera']),
-    )
+def load_photo(obj: dict) -> Photo:
+  return Photo(
+    id=obj['id'],
+    sol=obj['sol'],
+    img_src=obj['img_src'],
+    earth_date=parse_date(obj['earth_date']),
+    camera_id=load_camera(obj['camera']),
+  ).save()
 
-  def load_camera(obj: dict) -> Camera:
-    return Camera(
-      id=obj['id'],
-      name=obj['name'],
-      full_name=obj['full_name'],
-      rover_id=Rover(id=obj['rover_id']),
-    ).save()
+def load_camera(obj: dict) -> Camera:
+  return Camera(
+    id=obj['id'],
+    name=obj['name'],
+    full_name=obj['full_name'],
+    rover_id=Rover(id=obj['rover_id']),
+  ).save()
 
-  def load_rover(obj: dict) -> Rover:
-    return Rover(
-      id=obj['id'],
-      name=obj['name'],
-      landing_date=parse_date(obj['landing_date']),
-      launch_date=parse_date(obj['launch_date']),
-      status=obj['status'],
-      max_sol=obj['max_sol'],
-      total_photos=obj['total_photos']
-    )
-
-  return [
-    load_photo(obj).save()
-    for obj in photos
-  ]
-  pass
+def load_rover(obj: dict) -> Rover:
+  return Rover(
+    id=obj['id'],
+    name=obj['name'],
+    landing_date=parse_date(obj['landing_date']),
+    launch_date=parse_date(obj['launch_date']),
+    status=obj['status'],
+    max_sol=obj['max_sol'],
+    max_date=obj['max_date'],
+    total_photos=obj['total_photos'],
+  ).save()
 
 def index(request):
-  photos = api.getPhotos({
-    'sol': 1000,
-    'page': 1
-  })
-  loaded = load_json(photos)
-  return HttpResponse( loaded)
+  rovers = api.get_rovers()
+  rover_models = [
+    load_rover(rover)
+    for rover in rovers
+  ]
+
+  photos = api.get_photos(
+    rover = 'curiosity',
+    sol = 1000,
+    page = 1,
+  )
+
+  photo_models = [
+    load_photo(photo)
+    for photo in photos
+  ]
+
+  return HttpResponse((rover_models, photo_models))
