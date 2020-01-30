ca8b014e7b00662ebe7b8747653e30d7efdb83dc
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-01-29 20:14:15 -0500

    Basic layout

--- react-slideshow/src/api.js
+++ react-slideshow/src/api.js
@@ -1,8 +1,8 @@
 export const rovers = [
-  Curiosity,
-  Oppurtunity,
-  Spirit,
-]
+  'Curiosity',
+  'Oppurtunity',
+  'Spirit',
+];
 
 export const camera =
-[{"name":"FHAZ","full_name":"Front Hazard Avoidance Camera"},{"name":"NAVCAM","full_name":"Navigation Camera"},{"name":"PANCAM","full_name":"Panoramic Camera"},{"name":"MINITES","full_name":"Miniature Thermal Emission Spectrometer (Mini-TES)"},{"name":"ENTRY","full_name":"Entry, Descent, and Landing Camera"},{"name":"RHAZ","full_name":"Rear Hazard Avoidance Camera"}]
+[{"name":"FHAZ","full_name":"Front Hazard Avoidance Camera"},{"name":"NAVCAM","full_name":"Navigation Camera"},{"name":"PANCAM","full_name":"Panoramic Camera"},{"name":"MINITES","full_name":"Miniature Thermal Emission Spectrometer (Mini-TES)"},{"name":"ENTRY","full_name":"Entry, Descent, and Landing Camera"},{"name":"RHAZ","full_name":"Rear Hazard Avoidance Camera"}];
--- react-slideshow/src/components/Slideshow.js
+++ react-slideshow/src/components/Slideshow.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Slideshow.css';
+import { rovers, cameras } from '../api';
 
 const Slideshow = () => {
-  const url = 'https://cdn.theatlantic.com/assets/media/img/mt/2019/07/PIA22486_MAIN/lead_720_405.jpg?mod=1562072594'
+  const url = 'https://cdn.theatlantic.com/assets/media/img/mt/2019/07/PIA22486_MAIN/lead_720_405.jpg?mod=1562072594';
+  const [rover, setRover] = useState('');
+  const [cameras, setCameras] = useState([]);
+
+  const onChange = event => {
+    console.log(event)
+    setRover(event.target.value);
+  }
   return (
     <div className="rover-slideshow">
+      <div className="control-panel">
+      <select onChange={onChange}>
+        {rovers.map((rover, index) => (
+          <option key={index}>{rover}</option>
+        ))
+        }
+      </select>
+
+      <select disabled>
+         {cameras.map((camera, index) => (
+          <option key={index}>{camera}</option>
+        ))}
+      </select>
+
+      </div>
+
       <img className="rover-photo" src={url} />
     </div>
   );
