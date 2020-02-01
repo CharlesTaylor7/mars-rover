2360592a2c21345883d219e85f3d31131f53114c
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-02-01 15:15:49 -0500

    Dodge CORS by proxying requests to django

--- react-slideshow/src/api.js
+++ react-slideshow/src/api.js
@@ -7,9 +7,6 @@ export const rovers = [
 export const cameras = [{ name: 'FHAZ', full_name: 'Front Hazard Avoidance Camera' }, { name: 'NAVCAM', full_name: 'Navigation Camera' }, { name: 'PANCAM', full_name: 'Panoramic Camera' }, { name: 'MINITES', full_name: 'Miniature Thermal Emission Spectrometer (Mini-TES)' }, { name: 'ENTRY', full_name: 'Entry, Descent, and Landing Camera' }, { name: 'RHAZ', full_name: 'Rear Hazard Avoidance Camera' }];
 
 /* eslint-disable-next-line */
-console.log(process.env.REACT_APP_PHOTO_SERVER);
-const url = 'http://localhost:8000/photos';
-
 export const fetchPhotos = ({ rover, camera }) =>
-  fetch(url, { mode: 'no-cors'})
+  fetch('photos', { mode: 'no-cors'})
   .then(res => res.json());
