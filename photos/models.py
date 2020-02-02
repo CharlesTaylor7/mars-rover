538bcf31a0edda2995d6d671629bbf97bb7563c5
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-02-01 20:03:01 -0500

    Index models

--- photos/models.py
+++ photos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Rover(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(db_index=True, max_length=200)
     landing_date = models.DateField(default=None)
     launch_date = models.DateField(default=None)
     status = models.CharField(max_length=50)
@@ -14,7 +14,7 @@ class Rover(models.Model):
 
 class Camera(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(db_index=True, max_length=50)
     full_name = models.CharField(max_length=200)
     rover = models.ForeignKey(Rover, on_delete=models.SET_NULL, null=True)
 
@@ -23,5 +23,5 @@ class Photo(models.Model):
     id = models.IntegerField(primary_key=True)
     sol = models.IntegerField()
     img_src = models.CharField(max_length=200)
-    earth_date = models.DateField(default=None)
+    earth_date = models.DateField(db_index=True, default=None)
     camera = models.ForeignKey(Camera, on_delete=models.SET_NULL, null=True)
