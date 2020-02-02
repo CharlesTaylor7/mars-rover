48e8f85743efa5c829758a5b5c0825129e07befc
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-02-01 20:36:45 -0500

    Finish route

--- photos/views.py
+++ photos/views.py
@@ -4,30 +4,42 @@ from .models import Photo, Camera, Rover
 from typing import List
 from django.http import JsonResponse
 from psycopg2 import sql
+from django.db import connection
 
 
-def get_photos_with_raw_SQL(rover, camera):
+def safe_SQL_query(rover, camera):
     return sql.SQL("""
-        SELECT photos_photo.img_src, photos_photo.earth_date
-        FROM photos_photo
-        INNER JOIN photos_camera
-            ON photos_photo.camera_id=photos_camera.id
-        INNER JOIN photos_rover
-            ON photos_camera.rover_id=photos_rover.id
-        WHERE   photos_rover.name='{rover}'
-        AND photos_camera.name='{camera}';
+        SELECT
+            photos_photo.img_src,
+            photos_photo.earth_date
+        FROM
+            photos_photo
+        INNER JOIN
+            photos_camera ON photos_photo.camera_id = photos_camera.id
+        INNER JOIN
+            photos_rover ON photos_camera.rover_id = photos_rover.id
+        WHERE
+            photos_rover.name = '{rover}'
+        AND
+            photos_camera.name = '{camera}'
+        ORDER BY
+            photos_photo.earth_date ASC;
     """).format(
         rover=sql.Identifier(rover),
         camera=sql.Identifier(camera),
     )
 
 
+def get_photos_with_raw_SQL(rover, camera):
+    with connection.cursor() as cursor:
+        cursor.execute(safe_SQL_query(rover, camera))
+        columns = [col[0] for col in cursor.description]
+        return [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
+
+
 def index(request, rover, camera):
-    print(rover)
-    print(camera)
-    response = [{
-        'img_src': 'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG',
-    }, {
-        'img_src': 'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FRB_486265257EDR_F0481570FHAZ00323M_.JPG',
-    }]
+    response = get_photos_with_raw_SQL(rover, camera)
     return JsonResponse(response, safe=False)
