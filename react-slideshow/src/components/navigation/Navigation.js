31097fbd1340bb9bfc913e7e487415339aca089d
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-02-02 08:44:50 -0500

    Tweak margins

--- react-slideshow/src/components/navigation/Navigation.js
+++ react-slideshow/src/components/navigation/Navigation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ArrowButton from './ArrowButton';
+import './Navigation.css';
 
 const Navigation = ({ prevPhoto, nextPhoto, prevDisabled, nextDisabled }) => (
-  <div className="navigation-buttons">
+  <div className="navigation">
     <ArrowButton direction="left" handleClick={prevPhoto} disabled={prevDisabled} />
     <ArrowButton direction="right" handleClick={nextPhoto} disabled={nextDisabled} />
   </div>
