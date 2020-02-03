3fff83f61250c26216074664926c6688a64ef486
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-02-02 19:28:20 -0500

    Query by both SQL & ORM; Include in integration test

--- react-slideshow/src/api.js
+++ react-slideshow/src/api.js
@@ -6,4 +6,6 @@ export const rovers = [
 
 export const cameras = [{ name: 'FHAZ', full_name: 'Front Hazard Avoidance Camera' }, { name: 'NAVCAM', full_name: 'Navigation Camera' }, { name: 'PANCAM', full_name: 'Panoramic Camera' }, { name: 'MINITES', full_name: 'Miniature Thermal Emission Spectrometer (Mini-TES)' }, { name: 'ENTRY', full_name: 'Entry, Descent, and Landing Camera' }, { name: 'RHAZ', full_name: 'Rear Hazard Avoidance Camera' }];
 
-export const fetchPhotos = ({ rover, camera }) => fetch(`photos/${rover}/${camera}`).then((res) => res.json());
+export const fetchPhotos = ({ rover, camera, method = 'ORM' }) => fetch(
+  `photos/${rover}/${camera}/?method=${method}`,
+).then((res) => res.json());
