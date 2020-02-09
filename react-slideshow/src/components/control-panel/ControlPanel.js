7d08ac1fddee959f5c0751ad58e2af0a27536328
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-02-09 12:12:33 -0500

    Move navigation to control panel

--- react-slideshow/src/components/control-panel/ControlPanel.js
+++ react-slideshow/src/components/control-panel/ControlPanel.js
@@ -1,17 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import './ControlPanel.css';
 import { rovers, cameras } from '../../api';
 import Dropdown from './Dropdown';
+import Navigation from '../navigation/Navigation';
 
 const roverOptions = rovers.map((rover) => ({ value: rover, display: rover }));
 const cameraOptions = cameras.map((camera) => ({ value: camera.name, display: camera.full_name }));
 
 const ControlPanel = ({
-  setRover, setCamera,
+  setRover, setCamera, navigation,
 }) => (
   <div className="control-panel">
     <Dropdown label="Rover" handleSelect={setRover} options={roverOptions} />
     <Dropdown label="Camera" handleSelect={setCamera} options={cameraOptions} />
+    <Navigation {...navigation} />
   </div>
 );
 
