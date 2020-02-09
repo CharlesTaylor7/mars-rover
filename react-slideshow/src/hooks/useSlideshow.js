197dd92e6515d82dafc6c31dfc615031dd61b184
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-02-08 22:18:21 -0500

    Prefetch photos

--- react-slideshow/src/hooks/useSlideshow.js
+++ react-slideshow/src/hooks/useSlideshow.js
@@ -1,6 +1,12 @@
 import { useState, useCallback, useEffect } from 'react';
 import { fetchPhotos, cameras, rovers } from '../api';
 
+const loadImage = (src) => new Promise((resolve) => {
+  const image = document.createElement('img');
+  image.src = src;
+  image.addEventListener('load', () => resolve(image));
+});
+
 export default () => {
   const [rover, setRover] = useState(rovers[0]);
   const [camera, setCamera] = useState(cameras[0].name);
@@ -8,15 +14,6 @@ export default () => {
   const [photoIndex, setPhotoIndex] = useState(0);
   const prevDisabled = photoIndex === 0;
   const nextDisabled = photoIndex === photos.length - 1;
-
-  useEffect(() => {
-    (async () => {
-      const newPhotos = await fetchPhotos({ rover, camera });
-      setPhotoIndex(0);
-      setPhotos(newPhotos);
-    })();
-  },
-  [rover, camera, setPhotos, setPhotoIndex]);
 
   const nextPhoto = useCallback(
     () => setPhotoIndex((i) => Math.min(i + 1, photos.length - 1)),
@@ -29,6 +26,26 @@ export default () => {
   );
 
   const photo = photos[photoIndex];
+
+  useEffect(() => {
+    (async () => {
+      const newPhotos = await fetchPhotos({ rover, camera });
+      setPhotoIndex(0);
+      setPhotos(newPhotos);
+    })();
+  },
+  [rover, camera, setPhotos, setPhotoIndex]);
+
+  useEffect(() => {
+    (async () => {
+      /* eslint-disable */
+      for (const photo of photos) {
+        await loadImage(photo.url);
+      }
+    })();
+  },
+  [photos]);
+
   return {
     setRover,
     setCamera,
