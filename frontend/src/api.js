0659948cacb471a6cccd1f2bb3b331273787eb65
Charles Taylor <charlestaylor95@gmail.com>
2025-01-08 23:27:45 -0500

    remove tests

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,7 @@
-export const rovers = [
-  'Curiosity',
-  'Opportunity',
-  'Spirit',
-];
+export const rovers = ["Curiosity", "Opportunity", "Spirit"];
 
-export const fetchCameras = (rover) => fetch(`mars_rover/cameras/?rover=${rover}`).then((res) => res.json());
+export const fetchCameras = (rover) =>
+  fetch(`mars_rover/cameras/?rover=${rover}`).then((res) => res.json());
 
-export const fetchPhotos = ({ rover, camera, method = 'ORM' }) => fetch(
-  `mars_rover/photos/${rover}/${camera}/?method=${method}`,
-).then((res) => res.json());
+export const fetchPhotos = ({ rover, camera }) =>
+  fetch(`mars_rover/photos/${rover}/${camera}`).then((res) => res.json());
