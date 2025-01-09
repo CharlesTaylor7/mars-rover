0891ec3648b0df9296063d42da2698757958e63b
Charles Taylor <charlestaylor95@gmail.com>
2025-01-08 23:32:55 -0500

    fix

--- backend/photos/management/commands/load_nasa_photos.py
+++ backend/photos/management/commands/load_nasa_photos.py
@@ -15,18 +15,6 @@ from . import _api as api
 from ._unbuffered import Unbuffered
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 class Command(BaseCommand):
     help = "Closes the specified poll for voting"
 
@@ -41,55 +29,72 @@ class Command(BaseCommand):
 
         rate_limit_message = "Api rate limit has been met; retry in an hour."
 
-        async def load_photos(session, pool, job):
-            async with pool.acquire() as connection:
-                for sol in range(job.sol, job.max_sol + 1):
-                    start_page = 1 if sol > job.sol else job.page
-                    for page in itertools.count(start=start_page):
-                        write(
-                            "rover = %s, sol = %d, page = %d" % (job.rover.name, sol, page)
-                        )
+        async def load_photos(session, job):
+            for sol in range(job.sol, job.max_sol + 1):
+                start_page = 1 if sol > job.sol else job.page
+                for page in itertools.count(start=start_page):
+                    write(
+                        "rover = %s, sol = %d, page = %d" % (job.rover.name, sol, page)
+                    )
 
-                        photos = await api.get_photos(
-                            session, rover=job.rover.name, sol=sol, page=page
-                        )
+                    photos = await api.get_photos(
+                        session, rover=job.rover.name, sol=sol, page=page
+                    )
 
-                        if photos == "RateLimited":
-                            write(rate_limit_message)
-                            job.sol = sol
-                            job.page = page
-                            await job.asave()
-                            return
+                    if photos == "RateLimited":
+                        write(rate_limit_message)
+                        job.sol = sol
+                        job.page = page
+                        await job.asave()
+                        return
 
-                        for photo in photos:
-                            await Camera(id=photo['camera']['id'], name=photo['camera']['name'], full_name=photo['camera']['full_name'], rover_id=photo['camera']['rover_id']).asave()
+                    for photo in photos:
+                        await Camera(
+                            id=photo["camera"]["id"],
+                            name=photo["camera"]["name"],
+                            full_name=photo["camera"]["full_name"],
+                            rover_id=photo["camera"]["rover_id"],
+                        ).asave()
 
-                            await Photo(id=photo['id'], sol=photo['sol'], img_src=photo['img_src'], earth_date=photo['earth_date'], camera_id=photo['camera']['id']).asave()
+                        await Photo(
+                            id=photo["id"],
+                            sol=photo["sol"],
+                            img_src=photo["img_src"],
+                            earth_date=photo["earth_date"],
+                            camera_id=photo["camera"]["id"],
+                        ).asave()
 
-                        if len(photos) < 25:
-                            break
+                    if len(photos) < 25:
+                        break
 
-        async def get_jobs(session, pool):
-            async with pool.acquire() as connection:
-                rovers = await api.get_rovers(session)
-                if rovers == "RateLimited":
-                    write(rate_limit_message)
-                    exit(0)
+        async def get_jobs(session):
+            rovers = await api.get_rovers(session)
+            if rovers == "RateLimited":
+                write(rate_limit_message)
+                exit(0)
 
-                jobs = []
-                for rover in rovers:
-                    await Rover(id=rover['id'], name=rover['name'], landing_date=rover['landing_date'], launch_date=rover['launch_date'], status=rover['status'], max_date=rover['max_date'], total_photos=rover['total_photos']).asave()
-                    job,_ = await Job.objects.select_related("rover").aget_or_create(rover_id=rover['id'],defaults={'max_sol': rover['max_sol']})
-                    jobs.append(job)
-                return jobs
+            jobs = []
+            for rover in rovers:
+                await Rover(
+                    id=rover["id"],
+                    name=rover["name"],
+                    landing_date=rover["landing_date"],
+                    launch_date=rover["launch_date"],
+                    status=rover["status"],
+                    max_date=rover["max_date"],
+                    total_photos=rover["total_photos"],
+                ).asave()
+                job, _ = await Job.objects.select_related("rover").aget_or_create(
+                    rover_id=rover["id"], defaults={"max_sol": rover["max_sol"]}
+                )
+                jobs.append(job)
+            return jobs
 
         async def populate_db():
             async with ClientSession(json_serialize=json.dumps) as session:
-                pool = await db.connection_pool()
-                jobs = await get_jobs(session, pool)
+                jobs = await get_jobs(session)
 
-                await asyncio.gather(*[load_photos(session, pool, job) for job in jobs])
-
+                await asyncio.gather(*[load_photos(session, job) for job in jobs])
 
         asyncio.run(populate_db())
 
