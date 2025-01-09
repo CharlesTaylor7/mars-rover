05528d143032798addcd3ea8e558c5b93ea4a42f
Charles Taylor <charlestaylor95@gmail.com>
2025-01-09 10:10:29 -0500

    WIP - transitions

--- frontend/src/components/Slideshow.js
+++ frontend/src/components/Slideshow.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import './Slideshow.css';
-import ControlPanel from './control-panel/ControlPanel';
-import useSlideshow from '../hooks/useSlideshow';
+import React, { useState, useEffect, useCallback } from "react";
+import "./Slideshow.css";
+import ControlPanel from "./control-panel/ControlPanel";
+import useOnKeyDown from "../hooks/useOnKeyDown";
+import { rovers, fetchCameras, fetchPhotos } from "../api";
 
-const Slideshow = () => {
-  const {
-    setRover, setCamera, cameras, navigation, photo,
-  } = useSlideshow();
+export default function Slideshow() {
+  const [rover, setRover] = useState(rovers[0]);
+  const [camera, setCamera] = useState();
+  const [cameras, setCameras] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const prevDisabled = photoIndex <= 0;
+  const nextDisabled = photoIndex >= photos.length - 1;
+
+  const slideToNext = useCallback(
+    () => setPhotoIndex((i) => Math.min(i + 1, photos.length - 1)),
+    [setPhotoIndex, photos],
+  );
+
+  const slideToPrevious = useCallback(
+    () => {
+      setPhotoIndex((i) => Math.max(i - 1, 0))
+      setClasses(["slide-rtl-in",])
+      setCurrentClass("slide-rtl-in")
+    }
+    [setPhotoIndex],
+  );
+
+  useOnKeyDown(({ key }) => {
+    if (key === "ArrowRight") {
+      slideToNext();
+    } else if (key === "ArrowLeft") {
+      slideToPrevious();
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchCameras(rover).then((cameras) => {
+      setCameras(cameras);
+      if (cameras.length) {
+        setCamera(cameras[0].name);
+      }
+    });
+  }, [rover]);
+
+  useEffect(() => {
+    if (camera === undefined) return;
+    fetchPhotos({ rover, camera }).then((photos) => {
+      setPhotoIndex(0);
+      setPhotos(photos);
+    });
+  }, [rover, camera, setPhotos, setPhotoIndex]);
 
   return (
     <div className="slideshow">
@@ -14,14 +58,29 @@ const Slideshow = () => {
         setRover={setRover}
         setCamera={setCamera}
         cameras={cameras}
-        navigation={navigation}
+        navigation={{
+          prevDisabled,
+          nextDisabled,
+          prevPhoto: slideToPrevious,
+          nextPhoto: slideToNext,
+        }}
       />
-      { photo
-        && (
-          <img className="rover-photo" src={photo.url} />
-        )}
+      <Photo
+        key={`slide-rtl-out-${photoIndex - 1}`}
+        photo={photos[photoIndex - 1]}
+        className="slide-out"
+      />
+      <Photo
+        key={`slide-rtl-in-${photoIndex}`}
+        photo={photos[photoIndex]}
+        className="slide-in"
+      />
     </div>
   );
-};
+}
 
-export default Slideshow;
+function Photo(props) {
+  return props.photo ? (
+    <img className={`rover-photo ${props.className}`} src={props.photo.url} />
+  ) : null;
+}
